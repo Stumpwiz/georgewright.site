24e4505e49868928dc811a4bc49f32b3e052ac50
Geo <geo@loyola.edu>
2025-11-07 20:15:18 -0500

    Streamlined the entire site.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// noinspection JSUnusedGlobalSymbols,ES6ConvertVarToLetConst,JSUnresolvedReference
+
 /*
 	Miniport by HTML5 UP
 	html5up.net | @ajlkn
@@ -6,29 +8,59 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+    var	$window = $(window),
+        $body = $('body'),
+        $nav = $('#nav');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge:      [ '1281px',  '1680px' ],
+        large:       [ '1025px',  '1280px' ],
+        mediumLarge: [ '981px',   '1024px' ],
+        medium:      [ '737px',   '980px'  ],
+        small:       [ '601px',   '736px'  ],
+        xsmall:      [ null,      '600px'  ]
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+    // Scrolly.
+    $('#nav a, .scrolly').scrolly({
+        speed: 1000,
+        offset: function() { return $nav.height(); }
+    });
+
+    // Cache mobile menu selectors
+    var $hamburgerIcon = $('.hamburger-icon');
+    var $navContainer = $('#nav ul.container');
+
+    // Mobile Menu Toggle
+    $hamburgerIcon.on('click', function() {
+        var isExpanded = $(this).attr('aria-expanded') === 'true';
+        $(this).toggleClass('active');
+        $(this).attr('aria-expanded', !isExpanded);
+        $navContainer.toggleClass('active');
+    });
+
+    // Close the mobile menu when a navigation item is clicked
+    $navContainer.find('a').on('click', function() {
+        $hamburgerIcon.removeClass('active');
+        $hamburgerIcon.attr('aria-expanded', 'false');
+        $navContainer.removeClass('active');
+    });
+
+    // Close the mobile menu when clicking outside
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#nav').length) {
+            $hamburgerIcon.removeClass('active');
+            $hamburgerIcon.attr('aria-expanded', 'false');
+            $navContainer.removeClass('active');
+        }
+    });
 
 })(jQuery);
